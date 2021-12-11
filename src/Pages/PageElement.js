5c1ba34ae867aeb8f1a0e764d6080845cc761cbf
mo13561 <91287581+mo13561@users.noreply.github.com>
2021-12-11 13:26:29 +0000

    overflowx

--- src/Pages/PageElement.js
+++ src/Pages/PageElement.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 export const Central = styled.div`
     height: auto;
-    width: 100%fit-content; 
+    width: 100%; 
     margin: 0 auto;
+    overflow: hidden;
 
     @media screen and (width: 280px) {
         width: 90mm;
