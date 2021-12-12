7739881e0fcdc00d07f34b2d4a4e47b38d98367e
mo13561 <91287581+mo13561@users.noreply.github.com>
2021-12-12 15:46:08 +0000

    twitter

--- src/components/Info/Data.js
+++ src/components/Info/Data.js
@@ -23,7 +23,7 @@ export const dataObject2 = {
     topline: '',
     headline: 'CONTACT',
     headloan: 'US',
-    description: '/*INSERT TWITTER HANDLE HERE.*/',
+    description: '<https://twitter.com/mo13562 />',
     buttonLabel: 'find out more',
     bottomline: 'IF YOU HAVE A QUERY',
     imgStart: true,
