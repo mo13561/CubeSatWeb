ae220930c42bce9f6e6bdc79b3f41a708868b24e
mo13561 <91287581+mo13561@users.noreply.github.com>
2021-12-12 15:38:52 +0000

    bookings

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -1,20 +1,91 @@
-import React from "react";
-import {ForumContainer, MainText, EmailBox, CommentBox, SubmitBox, NameBox, DateBox, T1} from "./BookingsElements";
+import {ForumContainer, MainText, EmailBox, CommentBox, SubmitBox, NameBox, BookingsLogo, T1, MainBox, StyledDatePickerWrapper, Box} from "./BookingsElements";
+import React, { useState } from "react";
+import "react-dates/initialize";
+import { SingleDatePicker } from "react-dates";
+import moment from "moment";
+import "react-dates/lib/css/_datepicker.css";
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <NameBox
+        ref={this.inputRef}
+        placeholder={this.props.placeholder}
+        onMouseOver={() => {
+          this.inputRef.current.focus()
+        }}
+        onMouseLeave={() => {
+          this.inputRef.current.blur()
+        }}
+      />
+    );
+  }
+}
+
+class EmailForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <EmailBox
+        ref={this.inputRef}
+        placeholder={this.props.placeholder}
+        onMouseOver={() => {
+          this.inputRef.current.focus()
+        }}
+        onMouseLeave={() => {
+          this.inputRef.current.blur()
+        }}
+      />
+    );
+  }
+}
+
+class DatePicker extends React.Component {
+  state = {
+    focused: false,
+    date: moment()
+  };
+  render() {
+    return (
+      <StyledDatePickerWrapper>
+        <SingleDatePicker
+          numberOfMonths={1}
+          onDateChange={date => this.setState({ date })}
+          onFocusChange={({ focused }) => this.setState({ focused })}
+          focused={this.state.focused}
+          date={this.state.date}
+        />
+      </StyledDatePickerWrapper>
+    );
+  }
+}
  
 const BookingsSection = () => {
     return (
       <>
-      <MainText>Blah blah blah</MainText>
+      
+      <MainBox>
+      <BookingsLogo to="/">Polaris</BookingsLogo>
+      <MainText>Book polaris now!</MainText>
         <ForumContainer> 
-          <NameBox placeholder="Name" />
-          <EmailBox placeholder="Email" />
-          <T1>Proposed date of your booking</T1>
-          <DateBox />
+          <NameForm placeholder="Name" />
+          <EmailForm placeholder="Email" />
+          <Box><T1>Proposed date of your booking:</T1><DatePicker /></Box>
           <CommentBox placeholder="Any additional comments about your booking" />
-          <SubmitBox />
+          <SubmitBox>Submit</SubmitBox>
         </ForumContainer>
+      </MainBox>
       </>
     );
 }; 
- 
+
 export default BookingsSection;
