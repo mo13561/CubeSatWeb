a2fe54aaa4d643cb180f48bf6338da021d1f04af
mo13561 <91287581+mo13561@users.noreply.github.com>
2022-01-03 22:58:44 +0000

    crypto

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -49,7 +49,7 @@ export const SidebarLink = styled(LinkS)`
     cursor: pointer;
 
     &:hover {
-        color: yellowgreen;
+        color: papayawhip;
         transition: 0.2s ease-in-out;
     }
 `;
